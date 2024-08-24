4c09540c962bef2734befc14c4810aa4c65199f7
SkrzypMajster <skrzypmajster@gmail.com>
2024-08-24 16:56:03 +0200

    feat: add user authentication logic to Auth.js credentials provider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 
+import { userAuthRepository } from "@/repositories/UserAuth.repository";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Credentials({
@@ -8,10 +10,22 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           email: { label: "Email", required: true },
           password: { label: "Password", type: "password", required: true },
         },
-        async authorize() {
-          return null;
+        async authorize({ email, password }) {
+          if (!email || !password) {
+            return null;
+          }
 
-          /// authorize login
+          const user = await userAuthRepository.findUserByCredentials(email as string, password as string);
+
+          if (!user) {
+            return null;
+          }
+
+          return {
+            id: user.user.id,
+            email: user.email,
+            name: user.user.name,
+          };
         },
       }),
   ],
