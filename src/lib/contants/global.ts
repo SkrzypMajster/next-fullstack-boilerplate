a83764cfefca52200857c17c2b5a8f3d6d435f69
SkrzypMajster <skrzypmajster@gmail.com>
2025-10-22 10:39:54 +0200

    feat: remove unnecessary code and assets from create-next-app

---
+++ src/lib/contants/global.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = 'Next Fullstack Boilerplate';
+export const APP_DESCRIPTION =
+  'Boilerplate of the full-stack Next.js application created to start my side projects faster';
