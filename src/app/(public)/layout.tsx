bc35e99972d4105793d1c200908073b7921d9899
SkrzypMajster <skrzypmajster@gmail.com>
2025-10-24 13:13:34 +0200

    feat: refactor login form page

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -2,6 +2,8 @@ import { ReactNode } from 'react';
 import { redirect } from 'next/navigation';
 
 import { auth } from '@/auth';
+import { AppIcon } from '../_components/AppIcon';
+import { APP_NAME } from '@/lib/contants';
 
 type PublicRootLayoutProps = {
   children: ReactNode;
@@ -14,5 +16,20 @@ export default async function PublicRootLayout({ children }: PublicRootLayoutPro
     redirect('/');
   }
 
-  return <>{children}</>;
+  return (
+    <main className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex items-center justify-center gap-2 font-medium md:justify-start">
+          <AppIcon />
+          {APP_NAME}
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">{children}</div>
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        <div className="from-primary to-secondary absolute inset-0 h-full w-full bg-gradient-to-br" />
+      </div>
+    </main>
+  );
 }
