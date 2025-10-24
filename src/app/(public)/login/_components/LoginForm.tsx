d07a375159730455f2a49f91ad243879e48c4943
SkrzypMajster <skrzypmajster@gmail.com>
2025-10-24 13:13:34 +0200

    feat: refactor login form page

--- src/app/(public)/login/_components/LoginForm.tsx
+++ src/app/(public)/login/_components/LoginForm.tsx
@@ -1,20 +1,24 @@
 'use client';
-import { FormEventHandler, useState } from 'react';
 import { redirect } from 'next/navigation';
+import Link from 'next/link';
 
-import { useToast } from '@/context/toast';
 import { loginAction } from '@/actions/auth';
-import { APP_NAME } from '@/lib/contants';
+import { useToast } from '@/context/toast';
+import { useForm } from '@/hooks/useForm';
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
+import { Field, FieldDescription, FieldGroup } from '@/components/ui/field';
+import { InputField } from '@/app/_components/fields/InputField';
+
+import { loginFormDefaultValues, LoginFormSchema } from './LoginForm.schema';
 
 export const LoginForm = () => {
   const showToast = useToast();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const { control, handleSubmit, formState } = useForm({
+    formSchema: LoginFormSchema,
+    defaultValues: loginFormDefaultValues,
+  });
 
-  const handleFormSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
-    event.preventDefault();
+  const handleFormSubmit = handleSubmit(async ({ email, password }) => {
     const { isSuccess, errors } = await loginAction({ email, password });
 
     if (errors) {
@@ -27,35 +31,43 @@ export const LoginForm = () => {
     if (isSuccess) {
       redirect('/');
     }
-  };
+  });
 
   return (
-    <form
-      onSubmit={handleFormSubmit}
-      className="mx-4 flex w-full max-w-sm flex-col space-y-4 rounded-xl border border-white p-4"
-    >
-      <h1 className="text-center text-xl">Login to {APP_NAME}</h1>
-      <Input
-        required
-        name="email"
-        placeholder="Email"
-        autoComplete="email"
-        className="rounded-sm border border-white p-2"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <Input
-        required
-        name="password"
-        type="password"
-        placeholder="Password"
-        className="rounded-sm border border-white p-2"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button className="w-full rounded-sm border border-white py-1" type="submit">
-        Submit
-      </Button>
+    <form onSubmit={handleFormSubmit} className="flex flex-col gap-6">
+      <FieldGroup>
+        <div className="flex flex-col items-center gap-1 text-center">
+          <h1 className="text-2xl font-bold">Login to your account</h1>
+          <p className="text-muted-foreground text-sm text-balance">Enter your email below to login to your account</p>
+        </div>
+        <InputField control={control} name="email" label="Email" placeholder="john.doe@example.com" />
+        <InputField
+          control={control}
+          name="password"
+          type="password"
+          label="Password"
+          data-1p-ignore
+          data-lpignore="true"
+          data-protonpass-ignore="true"
+        />
+        <Field>
+          <Button
+            className="from-primary to-secondary bg-gradient-to-br hover:cursor-pointer"
+            type="submit"
+            disabled={!formState.isValid}
+          >
+            Login
+          </Button>
+        </Field>
+        <Field>
+          <FieldDescription className="text-center">
+            Don&apos;t have an account?{' '}
+            <Link href="/signup" className="underline underline-offset-4">
+              Sign up
+            </Link>
+          </FieldDescription>
+        </Field>
+      </FieldGroup>
     </form>
   );
 };
