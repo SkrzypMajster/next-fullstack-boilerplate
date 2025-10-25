18b02a833199d265e2219d1e09a3acc543a58d0a
SkrzypMajster <skrzypmajster@gmail.com>
2025-10-26 00:35:11 +0200

    feat: add MainContent component

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,1 +1,1 @@
-export { WorkflowIcon, LogOutIcon, LayoutDashboardIcon, XIcon } from 'lucide-react';
+export { WorkflowIcon, LogOutIcon, LayoutDashboardIcon, XIcon, MenuIcon, PanelLeftIcon } from 'lucide-react';
