d38f1440e7e537b22964b18db9212e5c5c07c246
SkrzypMajster <skrzypmajster@gmail.com>
2025-10-26 00:35:46 +0200

    feat: refactor AuthorizedPagesRootLayout and DashboardPage

--- src/app/(authorized)/page.tsx
+++ src/app/(authorized)/page.tsx
@@ -1,21 +1,7 @@
-import { auth } from '@/auth';
-import { LogoutButton } from '@/app/_components/layout/LogoutButton';
-
 export default async function DashboardPage() {
-  const session = await auth();
-
-  if (!session) {
-    return (
-      <main className="flex h-screen w-screen items-center justify-center">
-        <h1 className="text-3xl">Hello World!</h1>
-      </main>
-    );
-  }
-
   return (
-    <main className="flex h-screen w-screen flex-col items-center justify-center gap-4">
-      <h1 className="text-3xl">Hello {session.user?.name}!</h1>
-      <LogoutButton />
-    </main>
+    <div className="flex h-full min-h-[calc(100vh-64px)] w-full items-center justify-center">
+      <h1 className="text-3xl">Hello World!</h1>
+    </div>
   );
 }
