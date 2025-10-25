81a6ed3bc60c2d52e122407c9391892d65a69ffa
SkrzypMajster <skrzypmajster@gmail.com>
2025-10-25 02:43:40 +0200

    feat: add UserLabel component

---
+++ src/app/_components/UserAvatar.tsx
@@ -0,0 +1,32 @@
+import { cn } from '@/utils/cn';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+
+type UserAvatarSize = 'sm' | 'lg';
+
+type UserAvatarProps = {
+  name: string;
+  imageSrc: string | undefined;
+  size?: UserAvatarSize;
+};
+
+export const UserAvatar = ({ name, imageSrc, size = 'sm' }: UserAvatarProps) => {
+  const initials = (() => {
+    const [firstName, lastName] = name.split(' ');
+    const firstNameInitial = firstName[0].toUpperCase();
+    const lastNameInitial = lastName ? lastName[0].toLocaleUpperCase() : '';
+
+    return `${firstNameInitial}${lastNameInitial}`;
+  })();
+
+  return (
+    <Avatar
+      className={cn({
+        'h-5 w-5': size === 'sm',
+        'h-20 w-20': size === 'lg',
+      })}
+    >
+      <AvatarImage src={imageSrc} alt={name} />
+      <AvatarFallback>{initials}</AvatarFallback>
+    </Avatar>
+  );
+};
---
+++ src/app/_components/layout/UserLabel.tsx
@@ -0,0 +1,17 @@
+'use client';
+import { useSession } from '@/auth';
+import { UserAvatar } from '@/app/_components/UserAvatar';
+
+export const UserLabel = () => {
+  const { data } = useSession();
+
+  const userName = data?.user?.name || '';
+  const userImageSrc = data?.user?.image || undefined;
+
+  return (
+    <div className="flex items-center gap-3 rounded-2xl px-3 py-2 text-sm font-medium">
+      <UserAvatar name={userName} imageSrc={userImageSrc} />
+      <span>{userName}</span>
+    </div>
+  );
+};
