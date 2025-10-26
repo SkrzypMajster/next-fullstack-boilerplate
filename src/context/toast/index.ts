e7ca51490cd36ed4bf07d311b8d672e941c538f6
SkrzypMajster <skrzypmajster@gmail.com>
2025-10-26 02:37:26 +0200

    feat: remove unnecessary ToastContext

--- src/context/toast/index.ts
+++
@@ -1,2 +0,0 @@
-export { useToast } from './useToast';
-export { ToastContextProvider } from './ToastContextProvider';
--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,15 +1,9 @@
 import { ReactNode } from 'react';
 
-import { ToastContextProvider } from '@/context/toast';
-
 import { ThemeProvider } from './theme/ThemeProvider';
 
 type AppProvidersProps = {
   children: ReactNode;
 };
 
-export const AppProviders = ({ children }: AppProvidersProps) => (
-  <ThemeProvider>
-    <ToastContextProvider>{children}</ToastContextProvider>
-  </ThemeProvider>
-);
+export const AppProviders = ({ children }: AppProvidersProps) => <ThemeProvider>{children}</ThemeProvider>;
