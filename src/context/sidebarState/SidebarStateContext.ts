12abb3f9f15b10c157aae4e6a5b9093bb36a6ac3
SkrzypMajster <skrzypmajster@gmail.com>
2025-10-28 09:52:00 +0100

    feat: add sidebarItems to SidebarStateContext

--- src/context/sidebarState/SidebarStateContext.ts
+++ src/context/sidebarState/SidebarStateContext.ts
@@ -1,8 +1,17 @@
 'use client';
 import { createContext } from 'react';
 
+import { Icon } from '@/components/icons';
+
+type SidebarItem = {
+  path: string;
+  name: string;
+  IconComponent: Icon;
+};
+
 type SidebarStateContextValue = {
   sidebarOpen: boolean;
+  sidebarItems: SidebarItem[];
   handleSidebarOpen: (open: boolean) => void;
 };
 
--- src/context/sidebarState/SidebarStateContextProvider.tsx
+++ src/context/sidebarState/SidebarStateContextProvider.tsx
@@ -1,6 +1,9 @@
 'use client';
 import { ReactNode, useState } from 'react';
 
+import { useSession } from '@/auth';
+import { routes } from '@/routing';
+
 import { SidebarStateContext } from './SidebarStateContext';
 
 type SidebarStateContextProviderProps = {
@@ -8,13 +11,32 @@ type SidebarStateContextProviderProps = {
 };
 
 export const SidebarStateContextProvider = ({ children }: SidebarStateContextProviderProps) => {
+  const { data } = useSession();
   const [sidebarOpen, setSidebarOpen] = useState(true);
+
+  const sidebarItems = routes
+    .filter((route) => {
+      if (route.hidden) {
+        // Hiding routes specified to be hidden in sidebar
+        return false;
+      }
+
+      if (!data?.user.role || !route.role.includes(data?.user.role)) {
+        // Hiding routes not available for current user role;
+        return false;
+      }
+
+      return true;
+    })
+    .map(({ name, path, IconComponent }) => ({ name, path, IconComponent }));
 
   const handleSidebarOpen = (open: boolean) => {
     setSidebarOpen(open);
   };
 
   return (
-    <SidebarStateContext.Provider value={{ sidebarOpen, handleSidebarOpen }}>{children}</SidebarStateContext.Provider>
+    <SidebarStateContext.Provider value={{ sidebarOpen, sidebarItems, handleSidebarOpen }}>
+      {children}
+    </SidebarStateContext.Provider>
   );
 };
