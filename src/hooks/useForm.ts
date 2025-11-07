b9c13a7fa5af53ad5bdfc8a3a00d4d290ca1fe14
SkrzypMajster <skrzypmajster@gmail.com>
2025-11-07 09:39:34 +0100

    fix: revert fix for formSchema type in useForm hook

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,12 +1,13 @@
-import { ZodType } from 'zod';
 import { useForm as useBaseForm, FieldValues, UseFormProps as UseBaseFormProps } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 type UseFormProps<T extends FieldValues> = Pick<UseBaseFormProps<T>, 'defaultValues' | 'mode'> & {
-  formSchema: ZodType<T, T>;
+  // TODO: Figure out how to use ZodType instead of any
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  formSchema: any;
 };
 
-export const useForm = <T extends FieldValues>({ defaultValues, mode, formSchema }: UseFormProps<T>) => {
+export const useForm = <T extends FieldValues>({ defaultValues, mode = 'onBlur', formSchema }: UseFormProps<T>) => {
   const resolver = zodResolver(formSchema);
 
   const { watch, control, handleSubmit, formState, reset } = useBaseForm<T>({
