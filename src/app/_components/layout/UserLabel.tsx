9f63017c5bcdac1aaff6872bfcf8bb7b1594041b
SkrzypMajster <skrzypmajster@gmail.com>
2025-11-07 11:21:43 +0100

    feat: create route handler for fetching user avatar image

--- src/app/_components/layout/UserLabel.tsx
+++ src/app/_components/layout/UserLabel.tsx
@@ -6,7 +6,7 @@ export const UserLabel = () => {
   const { data } = useSession();
 
   const userName = data?.user?.name || '';
-  const userImageSrc = data?.user?.image || undefined;
+  const userImageSrc = data?.user.id ? `assets/users/${data.user.id}/avatar` : undefined;
 
   return (
     <div className="flex items-center gap-3 rounded-2xl px-3 py-2 text-sm font-medium">
